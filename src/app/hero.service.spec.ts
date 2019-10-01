fefe9078b08c9206c2ad27a33b5d011d9d53d0ba
jaybenaim <benaimjacob@gmail.com>
2019-10-01 18:48:17 -0400

    initial commit

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+}
